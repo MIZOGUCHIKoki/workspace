2bffb1fbd149115d2bce97e92c9dde626427da27
MIZOGUCHI Koki <94270819+MIZOGUCHIKoki@users.noreply.github.com>
2024-07-03 01:34:04 +0900

    Update

---
+++ Roulette/hotel.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+ unsigned int a = 0;
+ unsigned int b = 0;
+ unsigned int parm = atoi(argv[1]);
+ while(1) {
+   srand(b + parm);
+   if ( b == parm ) break;
+   b++;
+   a = (unsigned int)rand();
+   printf("%10d\n", a);
+   switch((unsigned int)(a % 3)) {
+     case 0:
+      //printf("%3d : 桜スカイホテル 柏\n", b);
+      break;
+     case 1:
+      //printf("%3d : 三井ガーデンホテル\n", b);
+      break;
+     case 2:
+      //printf("%3d : KSEA\n", b);
+      break;
+   }
+ }
+}
