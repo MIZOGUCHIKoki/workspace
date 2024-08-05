e0d54220c3905efb4ea30c44debcfb13e9c40552
MIZOGUCHI Koki <94270819+MIZOGUCHIKoki@users.noreply.github.com>
2024-08-05 22:05:36 +0900

    Complete heapSort

--- SortAlgorithm/heapSort.c
+++ SortAlgorithm/heapSort.c
@@ -2,23 +2,37 @@
 #include "sort.h"
 
 void pushHeap(unsigned int heapTree[], unsigned int x, unsigned int size);
+unsigned int deleteMaximum(unsigned int heapTree[], unsigned int size);
 void pushHeapFromData(unsigned int data[], unsigned int heapTree[]);
+void deleteMaximumFromData(unsigned int heapTree[], unsigned int data[]);
 
 int main() {
 	unsigned int data[dataSize], original[dataSize], heapTree[dataSize] = {0};
 	createData(data, original);
 	printData(data);
 	pushHeapFromData(data, heapTree);
+	deleteMaximumFromData(heapTree, data);
+	printData(data);
+	verifySort(data, original);
 	return 0;
 }
 
 
 void pushHeapFromData(unsigned int data[], unsigned int heapTree[]) {
 	unsigned int i;
-	printDataVert(heapTree);
+	printDataVert(data);
 	printf("push heap\n");
 	for (i = 0; i < dataSize; i++) {
 		pushHeap(heapTree, data[i], i);
 	}
 	printDataVert(heapTree);
+}
+
+void deleteMaximumFromData(unsigned int heapTree[], unsigned int data[]) {
+	int i = dataSize - 1;
+	printf("delete maximum\n");
+	for (i = dataSize - 1; i >= 0; i--) {
+		data[i] = deleteMaximum(heapTree, i);
+	}
+	return;
 }
