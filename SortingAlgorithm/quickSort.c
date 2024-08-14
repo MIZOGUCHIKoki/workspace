69ae4eef9f16736f967e3e74c2992fa16be80e9d
MIZOGUCHI Koki <94270819+MIZOGUCHIKoki@users.noreply.github.com>
2024-08-14 17:18:44 +0900

    Update

--- SortingAlgorithm/quickSort.c
+++ SortingAlgorithm/quickSort.c
@@ -7,10 +7,8 @@ unsigned int partition(unsigned int data[], unsigned int left, unsigned int righ
 int main() {
 	unsigned int data[dataSize], original[dataSize];
 	createData(data, original);
-	printf("ORIGINAL\n >> ");
 	printDataVert(data);
 	printf("\n");
-	printf(">> CALL(0) quickSort(left = %u, right = %u)\n", 0, dataSize - 1);
 	printf("--- Sorting ---\n");
 	quickSort(data, 0, dataSize - 1);
 	printDataVert(data);
@@ -23,12 +21,12 @@ void quickSort(unsigned int data[], unsigned int left, unsigned int right) {
 	//printf(">>> left = %u, right = %u\n", left, right);
 	if (left < right) {
 		unsigned int pivod_index = partition(data, left, right);
-		printf("pivod_index = %d\n", pivod_index);
+		//printf("pivod_index = %d\n", pivod_index);
 		if (pivod_index != 0) { 
-			printf(">> CALL(1) quickSort(left = %u, right = %u)\n", left, pivod_index - 1);
+			//printf(">> CALL(1) quickSort(left = %u, right = %u)\n", left, pivod_index - 1);
 			quickSort(data, left, pivod_index - 1);
 		}
-		printf(">> CALL(2) quickSort(left = %u, right = %u)\n", pivod_index + 1, right);
+		//printf(">> CALL(2) quickSort(left = %u, right = %u)\n", pivod_index + 1, right);
 		quickSort(data, pivod_index + 1, right);
 	}
 }
@@ -41,29 +39,29 @@ unsigned int partition(unsigned int data[], unsigned int left, unsigned int righ
 	else if ((data[mid] >= data[left] && data[mid] <= data[right]) || (data[mid] >= data[right] && data[mid] <= data[left])) {k = mid;}
 	else {k = right;}
 	
-	printf("CALLED partition(), Data[%u] = %u is selected as a pivod value\n", k, data[k]);
+	//printf("CALLED partition(), Data[%u] = %u is selected as a pivod value\n", k, data[k]);
 	swap(data, k, right);
 	int i = left, j = right - 1;
 	//printDataVert(data);
 	while (1) {
 		if (i > j) { 
-			printf("Break while i = %u, j = %u\n", i, j);
+			//printf("Break while i = %u, j = %u\n", i, j);
 			break;
 		}
-		printf("debug point(0) i = %u, j = %u\n", i, j);
+		//printf("debug point(0) i = %u, j = %u\n", i, j);
 		while (data[i] < data[right]) { i++; }
-		printf("debug point(1) i = %u, j = %u\n", i, j);
+		//printf("debug point(1) i = %u, j = %u\n", i, j);
 		while (data[j] >= data[right] && j >= i) { j--; }
-		printf("debug point(2) i = %u, j = %u\n", i, j);
+		//printf("debug point(2) i = %u, j = %u\n", i, j);
 		if (i < j) {
 			swap(data, i, j);
-			printf("swapped data[%u] <-> data[%u]\n -> ", i, j);
+			//printf("swapped data[%u] <-> data[%u]\n -> ", i, j);
 			//printDataVert(data);
 		}
 	}       
-	printf("swapped as final process\n i = %u, j = %u\n", i , j);
+	//printf("swapped as final process\n i = %u, j = %u\n", i , j);
 	swap(data, i, right); // set pivod value to mid of the set.
 	//printDataVert(data);
-	printf("\n");
+	//printf("\n");
 	return i; // return the pivod value location
 }
